f41bf3f0bbd41f481a888f64eb698c36aace66b7
X-Engineer-001 <fabin1790@gmail.com>
2016-01-24 23:05:35 +0800

    test

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var ctx=canvas.getContext("2d");
 var tower1img=document.createElement("img");
 tower1img.src="images/tower1.jpg";
 function draw(){
-  ctx.drawlImage(tower1img,0,0);
+  ctx.drawImage(tower1img,0,0);
 }
 setInterval(draw,15)
