55f19bac63838bb0f2323c6691834bafe737401d
X-Engineer-001 <fabin1790@gmail.com>
2016-01-25 19:13:34 +0800

    test

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+var FPS=60
 var canvas=document.getElementById("gamecanvas");
 var ctx=canvas.getContext("2d");
-var tower1img=document.createElement("img");
-tower1img.src="images/tower1.jpg";
+var p1plane=document.createElement("img");
+p1plane.src="images/p1plane.png";
+var p2plane=document.createElement("img");
+p2plane.src="images/p2plane.png";
+var commonball=document.createElement("img");
+commonball.src="images/commonball.png";
+var rareball=document.createElement("img");
+rareball.src="images/rareball.png";
+var epicball=document.createElement("img");
+epicball.src="images/epicball.png";
+var legendaryball=document.createElement("img");
+legendaryball.src="images/legendaryball.png";
 function draw(){
-  ctx.drawImage(tower1img,0,0);
+  ctx.drawImage(p1plane,0,0);
 }
-setInterval(draw,15)
+setInterval(draw,1000/FPS);
