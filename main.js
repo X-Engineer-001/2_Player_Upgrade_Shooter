dde78512b7bf62e95507c88818ccac6300f65292
X-Engineer-001 <fabin1790@gmail.com>
2016-01-28 19:17:57 +0800

    shot!

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var FPS=60;
 var counter=0;
+var p1bulletspliceflag=0;
+var p2bulletspliceflag=0;
 var flag=0;
 var p1key=0;
 var p2key=0;
@@ -56,7 +58,7 @@ var p1={
   direction:{x:-1,y:0},
   movedirection:{x:0,y:0},
   attack:15,
-  critical:1.2,
+  critical:0.2,
   shot:1,
   absorb:0,
   armor:100,
@@ -69,7 +71,7 @@ var p2={
   direction:{x:1,y:0},
   movedirection:{x:0,y:0},
   attack:15,
-  critical:1.2,
+  critical:0.2,
   shot:1,
   absorb:0,
   armor:100,
@@ -82,17 +84,79 @@ function Wall(){
   this.width=Math.floor(Math.random()*31)+30;
   this.height=Math.floor(Math.random()*31)+30;
 }
+function FindAbsolutevValue(a){
+  if(a>=0){
+    return a;
+  }
+  if(a<0){
+    return (-1*a);
+  }
+}
 function p1bullet(){
   this.x=p1.x+7;
   this.y=p1.y+7;
   this.direction=p1.direction;
   this.move=function(){
-  }
+    if(IsCollidedMovingPointToSurfaceOrSurfaceToSurface(p2.x,p2.y,20,20,this.x,this.y,6,6)||
+    IsCollidedMovingPointToSurfaceOrSurfaceToSurface(p2.x,p2.y,20,20,
+      this.x-(((500/FPS)-3)*FindAbsolutevValue(this.direction.x)),
+      this.y-(((500/FPS)-3)*FindAbsolutevValue(this.direction.y)),
+      (((1000/FPS)-6)*FindAbsolutevValue(this.direction.x))+6,
+      (((1000/FPS)-6)*FindAbsolutevValue(this.direction.y))+6)
+    ){
+      var p2cost=p1.attack;
+      var p1cost=0;
+      var heal=0;
+      if(Math.floor(Math.random()*4)==0){
+        p2cost=p2cost+(p1.attack*p1.critical);
+      }
+      if(Math.floor(Math.random()*4)==0){
+        heal=p2cost*p1.absorb;
+      }
+      if(Math.floor(Math.random()*4)==0){
+        p1cost=p2cost*p2.retort;
+      }
+      p2.hp=p2.hp-p2cost;
+      p1.hp=p1.hp-(p1cost-heal);
+      p1bulletspliceflag=1;
+    }else{
+      this.x=this.x+(1000*this.direction.x/FPS);
+      this.y=this.y+(1000*this.direction.y/FPS);
+    }
+  };
 }
 function p2bullet(){
   this.x=p2.x+7;
   this.y=p2.y+7;
   this.direction=p2.direction;
+  this.move=function(){
+    if(IsCollidedMovingPointToSurfaceOrSurfaceToSurface(p1.x,p1.y,20,20,this.x,this.y,6,6)||
+    IsCollidedMovingPointToSurfaceOrSurfaceToSurface(p1.x,p1.y,20,20,
+      this.x-(((500/FPS)-3)*FindAbsolutevValue(this.direction.x)),
+      this.y-(((500/FPS)-3)*FindAbsolutevValue(this.direction.y)),
+      (((1000/FPS)-6)*FindAbsolutevValue(this.direction.x))+6,
+      (((1000/FPS)-6)*FindAbsolutevValue(this.direction.y))+6)
+    ){
+      var p1cost=p2.attack;
+      var p2cost=0;
+      var heal=0;
+      if(Math.floor(Math.random()*4)==0){
+        p1cost=p1cost+(p2.attack*p2.critical);
+      }
+      if(Math.floor(Math.random()*4)==0){
+        heal=p1cost*p2.absorb;
+      }
+      if(Math.floor(Math.random()*4)==0){
+        p2cost=p1cost*p1.retort;
+      }
+      p1.hp=p1.hp-p1cost;
+      p2.hp=p2.hp-(p2cost-heal);
+      p2bulletspliceflag=1;
+    }else{
+      this.x=this.x+(1000*this.direction.x/FPS);
+      this.y=this.y+(1000*this.direction.y/FPS);
+    }
+  };
 }
 function IsCollidedMovingPointToPointOrPointToSurface(x,y,targetx,targety,targetwidth,targetheight){
   if(x>=targetx&&
@@ -161,6 +225,14 @@ document.onkeydown=function(){
   if(keycode==83){
     p2key=83;
     p2.movedirection={x:0,y:1};
+  }
+  if(keycode==48){
+    var newbullet=new p1bullet();
+    p1bullets.push(newbullet);
+  }
+  if(keycode==86){
+    var newbullet=new p2bullet();
+    p2bullets.push(newbullet);
   }
   }
 }
@@ -236,30 +308,46 @@ function draw(){
     }
     p1.x=p1.x+(p1.movedirection.x*300/FPS);
     if(IsCollidedToWallsMovingPointOrSurface(p1.x,p1.y,20,20)||
-    IsCollidedToWallsMovingPointOrSurface(p1.x-((150/FPS)-10),p1.y-((150/FPS)-10),300/FPS,300/FPS)||
+    IsCollidedToWallsMovingPointOrSurface(p1.x-((150/FPS)-10),p1.y,300/FPS,20)||
     p1.x<20||p1.x>660){
       p1.x=p1.x-(p1.movedirection.x*300/FPS);
     }
     p1.y=p1.y+(p1.movedirection.y*300/FPS);
     if(IsCollidedToWallsMovingPointOrSurface(p1.x,p1.y,20,20)||
-    IsCollidedToWallsMovingPointOrSurface(p1.x-((150/FPS)-10),p1.y-((150/FPS)-10),300/FPS,300/FPS)||
+    IsCollidedToWallsMovingPointOrSurface(p1.x,p1.y-((150/FPS)-10),20,300/FPS)||
     p1.y<20||p1.y>660){
       p1.y=p1.y-(p1.movedirection.y*300/FPS);
     }
     p2.x=p2.x+(p2.movedirection.x*300/FPS);
     if(IsCollidedToWallsMovingPointOrSurface(p2.x,p2.y,20,20)||
-    IsCollidedToWallsMovingPointOrSurface(p2.x-((150/FPS)-10),p2.y-((150/FPS)-10),300/FPS,300/FPS)||
+    IsCollidedToWallsMovingPointOrSurface(p2.x-((150/FPS)-10),p2.y,300/FPS,20)||
     p2.x<20||p2.x>660){
       p2.x=p2.x-(p2.movedirection.x*300/FPS);
     }
     p2.y=p2.y+(p2.movedirection.y*300/FPS);
     if(IsCollidedToWallsMovingPointOrSurface(p2.x,p2.y,20,20)||
-    IsCollidedToWallsMovingPointOrSurface(p2.x-((150/FPS)-10),p2.y-((150/FPS)-10),300/FPS,300/FPS)||
+    IsCollidedToWallsMovingPointOrSurface(p2.x,p2.y-((150/FPS)-10),20,300/FPS)||
     p2.y<20||p2.y>660){
       p2.y=p2.y-(p2.movedirection.y*300/FPS);
     }
+    for(var i=0;i<p1bullets.length;i++){
+      p1bullets[i].move();
+      if(p1bulletspliceflag==1){
+        p1bullets.splice(i,1);
+        p1bulletspliceflag=0;
+      }else{
+        ctx.drawImage(p1bullet,p1bullets[i].x,p1bullets[i].y,6,6);
+      }
+    }
+    for(var i=0;i<p2bullets.length;i++){
+      p2bullets[i].move();
+      if(p2bulletspliceflag==1){
+        p2bullets.splice(i,1);
+        p2bulletspliceflag=0;
+      }else{
+        ctx.drawImage(p2bullet,p2bullets[i].x,p2bullets[i].y,6,6);
+      }
+    }
   }
-  counter++
-  console.log(counter);
 }
 setInterval(draw,1000/FPS);
